02c03870c4a8400e0ffc1060eba15ad3016225b6
Administrator <1791492943@qq.com>
2023-06-24 14:34:46 +0800

    通过文件绝对路径获取文件图标

--- src/main/java/com/example/controller/ImageController.java
+++ src/main/java/com/example/controller/ImageController.java
@@ -1,15 +1,25 @@
 package com.example.controller;
 
 import com.example.common.R;
+import com.example.utils.ImageUtil;
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/image")
 public class ImageController {
 
-    public R getImage() {
-        return null;
+    @GetMapping("/{absolutePath}")
+    public R getImage(@PathVariable String absolutePath, HttpServletResponse response) throws IOException {
+        ServletOutputStream outputStream = response.getOutputStream();
+        ImageUtil.getImage(absolutePath,outputStream);
+        return R.success("图片下载成功");
     }
 
 }
