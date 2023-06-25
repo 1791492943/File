47108a00694af813f5f61e01661fe1b47f766cb2
Administrator <1791492943@qq.com>
2023-06-25 20:58:54 +0800

    添加预览图片功能

--- src/main/java/com/example/controller/OnlinePreviewController.java
+++ src/main/java/com/example/controller/OnlinePreviewController.java
@@ -1,11 +1,15 @@
 package com.example.controller;
 
 import com.example.common.R;
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.*;
+import java.util.Arrays;
 
 @RestController
 @RequestMapping("/onlinePreview")
@@ -14,15 +18,31 @@ public class OnlinePreviewController {
     @GetMapping("/text")
     public R previewText(String absolutePath) throws IOException {
         File file = new File(absolutePath);
-        if(!file.exists()) return R.error("预览文件不存在");
+        if (!file.exists()) return R.error("预览文件不存在");
 
-        BufferedReader br = new BufferedReader(new FileReader(file));
-        String s;
-        StringBuffer sb = new StringBuffer();
-        while ((s = br.readLine()) != null){
-            sb.append(s).append("\n");
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String s;
+            StringBuffer sb = new StringBuffer();
+            while ((s = br.readLine()) != null) {
+                sb.append(s).append("\n");
+            }
+            return R.success(sb);
         }
-        return R.success(sb);
+    }
+
+    @GetMapping("/image")
+    public void previewImage(String absolutePath, HttpServletResponse response) throws IOException {
+        File file = new File(absolutePath);
+        if (!file.exists()) return;
+
+        response.setContentType("application/octet-stream");
+
+        try (
+                BufferedInputStream inputStream = new BufferedInputStream(new FileInputStream(file));
+                ServletOutputStream outputStream = response.getOutputStream();
+        ) {
+            IOUtils.copy(inputStream,outputStream);
+        }
     }
 
 }
